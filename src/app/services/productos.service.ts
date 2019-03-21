eb4cbe60a55eed38f01394637f3136a6588a5490
JonathanG <gb18006@ues.edu.sv>
2019-03-21 10:26:20 -0600

    Fin de la Seccion 7 VAMOS

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,20 +8,50 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[]=[];
+  productosFiltrado: Producto[]=[];
   constructor(private http: HttpClient) { 
     this.cargarProductos();
   }
 
   
   private cargarProductos(){
-    this.http.get('https://angular-html-fc7e9.firebaseio.com/productos_idx.json')
+    return new Promise(( resolve, reject) => {
+      this.http.get('https://angular-html-fc7e9.firebaseio.com/productos_idx.json')
     .subscribe((resp: Producto[]) => {
-      this.productos=resp;
+      this.productos = resp;
       this.cargando = false;
-      
-
-    });
+      resolve();
+      });
+     });
   }
 
-  
+  getProducto(id: string){
+    return this.http.get(`https://angular-html-fc7e9.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string){
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then(()=>{
+        //aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    }else{
+      //aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+    }
+
+  private filtrarProductos(termino: string){
+      
+      this.productosFiltrado = [];
+      termino = termino.toLocaleLowerCase();
+      this.productos.forEach(prod => {
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+        if(prod.categoria.indexOf(termino)>=0 || tituloLower.indexOf(termino)>=0){
+          this.productosFiltrado.push(prod);
+        }
+
+      });
+  }
 }
